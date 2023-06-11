851a746da3535d20c5202744749916239802af55
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-06-12 03:04:54 +0900

    invoke network call on Android

--- androidApp/src/main/java/com/tkhs0604/kmmsandbox/android/MainActivity.kt
+++ androidApp/src/main/java/com/tkhs0604/kmmsandbox/android/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tkhs0604.kmmsandbox.Greeting
@@ -19,7 +26,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    var text by remember { mutableStateOf("Loading") }
+                    LaunchedEffect(true) {
+                        text = try {
+                            Greeting().greet()
+                        } catch (e: Exception) {
+                            e.localizedMessage ?: "error"
+                        }
+                    }
+                    GreetingView(text)
                 }
             }
         }
--- shared/src/commonMain/kotlin/com/tkhs0604/kmmsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/tkhs0604/kmmsandbox/Greeting.kt
@@ -1,12 +1,30 @@
 package com.tkhs0604.kmmsandbox
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        return """
-            |Hello, ${platform.name}!
-            |There are only ${daysUntilNewYear()} days left until New Year! 🎆
-            """.trimMargin()
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    @Throws(Exception::class)
+    suspend fun greet(): String {
+        val rockets: List<RocketLaunch> =
+            httpClient.get("https://api.spacexdata.com/v4/launches").body()
+        val lastSuccessLaunch = rockets.last { it.launchSuccess == true }
+        return "The last successful launch was ${lastSuccessLaunch.launchDateUTC} 🚀"
     }
 }
