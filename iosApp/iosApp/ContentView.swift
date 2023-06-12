91d2cdbd0baec6a038695f0ee96596f000f90dd3
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-06-13 03:40:34 +0900

    invoke network call on iOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,35 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    @ObservedObject private(set) var viewModel: ViewModel
 
-	var body: some View {
-		Text(greet)
-	}
+    var body: some View {
+        Text(viewModel.text)
+    }
+}
+
+extension ContentView {
+    // declared as an extension to ContentView as they are closely connected
+    class ViewModel: ObservableObject {
+        @Published var text = "Loading..."
+        init() {
+            // suspending functions are available in it as functions with callbacks (completionHandler).
+            // any exceptions that are instances of the Exception class or its subclass will be propagated as NSError
+            Greeting().greet { greeting, error in
+                DispatchQueue.main.async {
+                    if let greeting = greeting {
+                        self.text = greeting
+                    } else {
+                        self.text = error?.localizedDescription ?? "error"
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+		ContentView(viewModel: ContentView.ViewModel())
 	}
 }
